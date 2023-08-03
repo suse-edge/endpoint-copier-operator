bb426dd04782c822e20389d6108f076b8d2c98c0
Kristian Zhelyazkov <kzhelyazkov@Kristians-MacBook-Pro.local>
2023-08-03 11:13:53 +0300

    Replace raw kubernetes manifests with a Helm chart

--- internal/controller/endpoints_controller.go
+++ internal/controller/endpoints_controller.go
@@ -47,10 +47,6 @@ type EndpointsReconciler struct {
 	ApiserverProtocol        string
 }
 
-//+kubebuilder:rbac:groups=core,resources=endpoints,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=core,resources=endpoints/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=core,resources=endpoints/finalizers,verbs=update
-
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
 // TODO(user): Modify the Reconcile function to compare the state specified by
